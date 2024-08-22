c13286f85a7a1499ae51dae2bbe980f7667ace28
Jieung Park <jieungp@sfu.ca>
2024-08-22 16:28:01 -0700

    Removed SQL

--- src/pages/ViewConcept.js
+++ src/pages/ViewConcept.js
@@ -1,10 +1,18 @@
-// import Head from 'next/head';
-// import ContentList from '../components/ContentList';
+import Head from 'next/head';
+import ContentList from '../components/ContentList';
 
-// function ViewConcept() {
-//     return (
-//         <ContentList table="concepts" />
-//     )
-// }
+function ViewConcept() {
+    return (
+        <>
+            <Head>
+                <title>Explore Languages | Code Learn Hub</title>
+                <meta name="description" content="List of Programming Languages" />
+            </Head>
+            <main>
+                {/* <ContentList content={content} /> */}
+            </main>
+        </>
+    )
+}
 
-// export default ViewConcept;
+export default ViewConcept;
